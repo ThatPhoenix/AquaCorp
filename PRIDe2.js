f58fd1c416a5e2911255a67170a922d1c7a1678e
coolpixire <theemaraldblock@gmail.com>
2018-06-25 19:31:07 +0100

    Add files via upload

---
+++ PRIDe2.js
@@ -0,0 +1,284 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+const Version = "2.0"
+const prefix = ("$")
+const sleep = require("sleep")
+client.on('ready', () => { client.user.setActivity('Helping Redstoneclaw101 Build Trident Enterprises And PRIDe Systems')  
+console.log('[BC7] [PROCESS] FREIND BOT Online')
+});
+const Authorized = ("Phoenix Redstoneclaw101")
+///////////////////////////////////////////////////////////////////////////////
+client.on('message', message => {
+	if (message.author.id === "338332694725263361") {
+		let msg = message.content;
+
+		if (msg.startsWith("$eval")) {
+   		msg = msg.substring("$eval ".length)
+   		msg = msg.replace(/```js/gi, "")
+   		msg = msg.replace(/```/gi, "")
+
+   		try {
+   			let result = eval(msg)
+
+    		if (typeof(result) == "undefined") {
+    			message.channel.send({
+    				embed: {
+    					description: "This code didn't return any value",
+    					fields: [
+    						{
+    							name: ":inbox_tray: Input",
+    							value: "```" + msg + "```"
+    						},
+    						{
+    							name: ":outbox_tray: Output",
+    							value: "Nothing here :innocent:"
+    						}
+  	  				],
+  	  				footer: {
+  	  					text: "PRIDeEval"
+  	  				}
+  	  			}
+  	  		})
+  	  	} else {
+  	  		message.channel.send({
+  	  			embed: {
+    					fields: [
+    						{
+    							name: ":inbox_tray: Input",
+    							value: "```" + msg + "```"
+    						},
+    						{
+	    						name: ":outbox_tray: Output",
+  	  						value: "```" + result + "```"
+    						}
+    					],
+    					footer: {
+    						text: "PRIDeEval"
+    					}
+	    			}
+  	  		})
+    		}
+   		} catch (e) {
+   			message.channel.send({
+  	  			embed: {
+  	  				description: ":dizzy_face: Something went wrong",
+    					fields: [
+    						{
+    							name: ":inbox_tray: Input",
+    							value: "```" + msg + "```"
+    						},
+    						{
+    							name: "Error content",
+    							value: "```" + e + "```"
+    						}
+    					],
+    					footer: {
+    						text: "PRIDeEval"
+    					}
+	    			}
+  	  		})
+   		}
+  	}
+	}
+})
+//////////////////////////////////
+client.on('message', message => {
+	if (message.author.id === "344785231879995393") {
+		let msg = message.content;
+
+		if (msg.startsWith(";eval")) {
+   		msg = msg.substring(";eval ".length)
+   		msg = msg.replace(/```js/gi, "")
+   		msg = msg.replace(/```/gi, "")
+
+   		try {
+   			let result = eval(msg)
+
+    		if (typeof(result) == "undefined") {
+    			message.channel.send({
+    				embed: {
+    					description: "This code didn't return any value",
+    					fields: [
+    						{
+    							name: ":inbox_tray: Input",
+    							value: "```" + msg + "```"
+    						},
+    						{
+    							name: ":outbox_tray: Output",
+    							value: "Nothing here :innocent:"
+    						}
+  	  				],
+  	  				footer: {
+  	  					text: "PRIDeEval"
+  	  				}
+  	  			}
+  	  		})
+  	  	} else {
+  	  		message.channel.send({
+  	  			embed: {
+    					fields: [
+    						{
+    							name: ":inbox_tray: Input",
+    							value: "```" + msg + "```"
+    						},
+    						{
+	    						name: ":outbox_tray: Output",
+  	  						value: "```" + result + "```"
+    						}
+    					],
+    					footer: {
+    						text: "PRIDeEval"
+    					}
+	    			}
+  	  		})
+    		}
+   		} catch (e) {
+   			message.channel.send({
+  	  			embed: {
+  	  				description: ":dizzy_face: Something went wrong",
+    					fields: [
+    						{
+    							name: ":inbox_tray: Input",
+    							value: "```" + msg + "```"
+    						},
+    						{
+    							name: "Error content",
+    							value: "```" + e + "```"
+    						}
+    					],
+    					footer: {
+    						text: "PRIDeEval"
+    					}
+	    			}
+  	  		})
+   		}
+  	}
+	}
+})
+////////////////////////////////////////////////////////////
+
+///////////////////////////////
+client.on('message', message => {
+    if (message.content.startsWith(prefix + "Botinfo")) {
+        let bicon = client.user.displayAvatarURL;
+        let botembed = new Discord.RichEmbed()
+        .setDescription("Bot Information")
+        .setColor("#6100ff")
+        .setThumbnail(bicon)
+        .addField("Bot Name", client.user.username)
+        .addField("Created On", client.user.createdAt);
+    
+        return message.channel.send(botembed);
+      }
+    
+});
+/////////////////////////////
+client.on('message', message => {
+    if (message.content === prefix +'Ping') {
+        message.channel.sendMessage('**Bringing the ball**')
+        .then(msg => {
+          msg.edit(`:ping_pong: Pong! (It took: :signal_strength: ${msg.createdTimestamp - message.createdTimestamp}ms to bring the ball!) **Websocket:** ${client.ping}`);
+        });
+    };
+});
+//////////////////////////////
+client.on('message', message => {
+    if (message.content === prefix +'Cmds') {
+        var embed = new Discord.RichEmbed();
+    embed.setTitle('CB7 Cmds')
+      embed.setDescription(`Shows Cmds For CB7 Bot`);
+      embed.setColor('#009000');
+      embed.addField('Cmds: Cmds [Shows This Message] Ping [Shows A Response Time] Eval [Only Authorized People Can Do This] serverinfo [shows server info] Botinfo [Shows  Bot info nothing else] Kick [Kicks a member]');
+      embed.addField('Vips: None :(')
+      embed.addField('For More Information Please Contact a Owner Of CB7')
+      embed.addField('Support Server: https://discord.gg/ZHhZWfE')
+      embed.setFooter("We Hope You Enjoy Your Time With CB7")
+      embed.setTimestamp()
+    
+      message.channel.sendEmbed(embed);
+    }
+    });
+//////////////////////////////
+client.on('message', message => {
+    if (message.content.startsWith(prefix + "serverinfo")) {
+        message.channel.sendMessage("**```ini\n[Server Information]\n" +
+            "Server Name = " + message.guild.name + "\n" +
+            "Members = " + message.guild.memberCount + "\n" +
+            "Owner = " + message.guild.owner.user.username + "\n" +
+            "ID = " + message.guild.ownerID + "\n" +
+            "Region = " + message.guild.region + "\n" +
+            "Icon URL = " + message.guild.iconURL + "\n```**");
+    }
+    });
+//////////////////////////////
+client.on('message', message => {
+    if (message.content.startsWith(prefix + "assistant")) {
+        message.channel.sendMessage("Senior what would you like me to do?");
+}
+    if (message.content.startsWith("Stats")) {
+        message.channel.sendMessage("Displaying Diagnostics")
+        message.channel.sendMessage("==DIAGNOSTICS==")
+message.channel.sendMessage(message.channel.send(`= STATISTICS =
+• Mem Usage  :: ${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)} MB
+• Users   :: ${(client.users.size)}
+• Node       :: ${process.version}`, {code: 'asciidoc'}))}
+    });
+    
+
+/////////////////////////////
+client.on('message', message => {
+    if (message.content.startsWith(prefix + "Stats")) {
+        message.channel.send(`= STATISTICS =
+      • Mem Usage  :: ${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)} MB
+      • Users      :: $client.users.size + "\n" +
+      • Node       :: ${process.version}`, {code: 'asciidoc'});
+    }
+    });
+///////////////////////////////
+client.on('message', message => {
+    if (message.content.startsWith(prefix + "Kick")) {
+        let messageArray = message.content.split(" ");
+        let args = messageArray.slice(1);
+    //!kick @Twisted Reason
+
+    let kUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
+    if(!kUser) return message.channel.send("Can't find user!");
+    let kReason = args.join(" ").slice(22);
+    if(!message.member.hasPermission("ADMINISTRATOR")) return message.channel.send("No can do pal!");
+    if(kUser.hasPermission("ADMINISTRATOR")) return message.channel.send("That person can't be kicked!");
+
+    let kickEmbed = new Discord.RichEmbed()
+    .setDescription("~Kick~")
+    .setColor("#e56b00")
+    .addField("Kicked User", `${kUser} with ID ${kUser.id}`)
+    .addField("Kicked By", `<@${message.author.id}> with ID ${message.author.id}`)
+    .addField("Kicked In", message.channel)
+    .addField("Tiime", message.createdAt)
+    .addField("Reason", kReason);
+
+    let kickChannel = message.guild.channels.find(`name`, "bot-logs");
+    if(!kickChannel) return message.channel.send("Can't find bot-logs channel.");
+
+    message.guild.member(kUser).kick(kReason);
+    kickChannel.send(kickEmbed);
+
+
+    return;
+  }
+}
+);
+
+///////////////
+client.on('message', message => {
+    if (message.author.id === "338332694725263361") {
+    if (message.content.startsWith(prefix + "Connect.Phoenix.Sys")) {
+        message.guild.channels.find('name', 'bot-logs').sendMessage("**[PHOENIX_PROCESS]** Accessing Phoenix Systems...")    
+        message.guild.channels.find('name', 'bot-logs').sendMessage("Password Enter:")
+        sleep.sleep(3)
+        message.guild.channels.find('name', 'bot-logs').sendMessage("Connected To Phoenix Systems And Security")
+    }
+    }
+});
+//////////////////////////////
+client.login(process.env.TOKEN)
+///////////////////////////////
