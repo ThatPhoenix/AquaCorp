415d44384f80b08a90ec27590c9447a9200e567a
Phoenix <theemaraldblock@gmail.com>
2019-01-07 20:43:19 +0000

    Update agree.js

--- cmds/agree.js
+++ cmds/agree.js
@@ -2,7 +2,7 @@ const Discord = require("discord.js");
 
 module.exports.run = async (Aqua, message, args) => {
 Aqua.users.get(`${message.author}`).send(`Hello!, you have agreed to the Rules of Aqua Corporation  and there for you must follow them. Enjoy!`)
-Aqua.users.get(`${message.author}`).addRole(role).catch(message.channel.send);
+Aqua.users.get(`${message.author}`).addRole(role)
   
        }
 
