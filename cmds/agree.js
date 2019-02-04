47b13a9d58e9ddfecc8adb2ce027cf73882407e0
Phoenix <theemaraldblock@gmail.com>
2019-02-04 16:30:34 +0000

    Update agree.js

--- cmds/agree.js
+++ cmds/agree.js
@@ -1,10 +1,18 @@
 const Discord = require("discord.js");
 var role = ("Members")
 
+
 module.exports.run = async (Aqua, message, args) => {
 Aqua.users.get(`${message.author}`).send(`Hello!, you have agreed to the Rules of Aqua Corporation  and there for you must follow them. Enjoy!`)
-Aqua.users.get(`${message.author}`).addRole(role)
-  
+
+let role = message.guild.roles.find("name", "Members");
+
+let member = message.member;
+
+message.channel.send("Verifying...")
+member.addRole(role).catch(console.error);
+message.channel.send(`Welcome to Aqua Corporation ${member}! If you still dont have access to the server please contact a admin. `) 
+
        }
 
 module.exports.help = {
