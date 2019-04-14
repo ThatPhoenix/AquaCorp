b0738c192e89b79320c4d8fc4989301eee780c87
Phoenix <theemaraldblock@gmail.com>
2019-04-14 15:53:52 +0100

    Update PRIDe2.js

--- PRIDe2.js
+++ PRIDe2.js
@@ -3,7 +3,7 @@ const fs = require("fs");
 const Aqua = new Discord.Client
 prefix = ("!")
 
-const start = new Discord.WebhookClient(("566997311406735413" , ("tkFWcBIr2L0KqRAIs4Mm4Qp9ClHWNcVjgq3IULI2Ppbs0-Yssfrs9SiFsWa83mNf5yxH"))
+const start = new Discord.WebhookClient(("566997311406735413") , ("tkFWcBIr2L0KqRAIs4Mm4Qp9ClHWNcVjgq3IULI2Ppbs0-Yssfrs9SiFsWa83mNf5yxH"))
 const error = new Discord.WebhookClient(("542022879919996940") , ("2AZH6h0E2hgm7illGxX6xzJQXB2p1msDqZHBLpNwyZByzG1rGGTyExKoBxQ0PO1H3XkM"))
 
 Aqua.on("message", message => {
