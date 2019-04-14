206b315ecd23464d7bb761d5bb58849c50fc02f2
Phoenix <theemaraldblock@gmail.com>
2019-04-14 16:03:41 +0100

    Create toolong.js

---
+++ cmds/toolong.js
@@ -0,0 +1,24 @@
+const Discord = require("discord.js");
+
+
+
+module.exports.run = async (Aqua, message, args) => {
+message.channel.send(`**System:** Starting System Restart `)
+message.channel.send("Please wait for the Connected to DiscordAPI message")
+
+
+
+
+
+
+process.exit();
+
+
+
+
+
+       }
+
+module.exports.help = {
+  name: "toolong"
+}
